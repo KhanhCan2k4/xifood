1c6baeb4a3e649539e8886305f5e0348e0bd6dfc
KhanhCan2k4 <levietkhanh2k4@gmail.com>
2024-04-15 08:28:47 +0700

    set up - add activities

---
+++ app/src/main/java/vn/edu/tdc/xifood/CartActivity.java
@@ -0,0 +1,13 @@
+package vn.edu.tdc.xifood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/vn/edu/tdc/xifood/OrderActivity.java
@@ -0,0 +1,13 @@
+package vn.edu.tdc.xifood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
